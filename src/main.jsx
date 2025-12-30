4834a69358ae2b6cbbddaf8008756853c6030212
Sushma <hssushma2004@gmail.com>
2025-12-30 06:19:35 +0530

    Fix routing by adding basename for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/legacy/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
